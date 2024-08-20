51be5831ecc82c51a54bf5862b7dda8ddf5da3ce
Amit Singh <amitsskingh@gmail.com>
2024-08-20 18:37:08 +0530

    update

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler.js")
 const notFoundError = require("./middleware/not-found.js")
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend's URL
+  origin: `${FRONTEND_URL}`, // Frontend's URL
   credentials: true,
 }
 
