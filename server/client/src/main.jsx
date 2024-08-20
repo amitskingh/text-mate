6ab53b6dd5f4a7bd0f1bbd38d5edcf3af0f0a4ef
Amit Singh <amitsskingh@gmail.com>
2024-08-20 17:46:08 +0530

    update

--- server/client/src/main.jsx
+++ server/client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Books from "./component/Books.jsx"
 import Layout from "./component/layout/Layout.jsx"
